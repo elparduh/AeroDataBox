926eaf549f115061024b59bdbda6ac6eb4ae9c39
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2022-12-01 10:52:55 -0600

    test

--- AeroDataBox/AppDelegate.swift
+++ AeroDataBox/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
-
+// application
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     window = UIWindow(frame: UIScreen.main.bounds)
